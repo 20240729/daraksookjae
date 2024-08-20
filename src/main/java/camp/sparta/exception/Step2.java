64e8b402ac53ee6d87944c7beeaf065cf50d7f6c
20240729 <yoohyeonsik@gmail.com>
2024-08-20 20:14:49 +0900

    step2 해결

--- src/main/java/camp/sparta/exception/Step2.java
+++ src/main/java/camp/sparta/exception/Step2.java
@@ -2,11 +2,15 @@ package camp.sparta.exception;
 
 public class Step2 {
     public static void main(String[] args) {
-        int[] list = {1, 2, 3};
+        try {
+            int[] list = {1, 2, 3};
 
-        int index = 4;
-        int element = list[index];
+            int index = 4;
+            int element = list[index];
 
-        System.out.println("결과 : " + element);
+            System.out.println("결과 : " + element);
+        }catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("배열 길이 초과");
+        }
     }
 }
